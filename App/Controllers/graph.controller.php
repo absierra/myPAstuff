53fda142b2af4571aec6db7202e6ab994b9be9d1
khrome <abbey@khrome.net>
2012-05-09 13:25:57 -0700

    graph data endpoint

--- App/Controllers/graph.controller.php
+++ App/Controllers/graph.controller.php
@@ -1,18 +1,28 @@
 <?php
-    $category = WebApplication::get('category');
-    $department = WebApplication::get('department');
-    $fund = WebApplication::get('fund');
-    $disc = array();
-    if($department) $disc[] = 'department_division=\''.$department.'\'';
-    if($fund) $disc[] = 'superfund_fund=\''.$fund.'\'';
-    if($category) $disc[] = 'ledger_type_ledger_description=\''.$category.'\'';
-    $results = Data::search('BudgetData', implode(' && ', $disc));
-    $data = array();
-    if( ! $fund ) $data['fund'] = array();
-    if( ! $department ) $data['department'] = array();
-    if( ! $category ) $data['category'] = array();
-    foreach($results as $item){
-        
+    $name = WebApplication::get('name');
+    $deep = strstr($name, ':')?true:false;
+    switch($incomingType = WebApplication::get('type')){
+        case 'fund': $uniqueType = 'superfund_fund'; break;
+        case 'department': $uniqueType = 'department_division'; break;
+        case 'category': $uniqueType = 'ledger_type_ledger_description'; break;
     }
-    $renderer->assign('data', $results);
+    if($deep){
+        $type = $uniqueType;
+    }else{
+        switch($incomingType = WebApplication::get('type')){
+            case 'fund': $type = 'superfund'; break;
+            case 'department': $type = 'department'; break;
+            case 'category': $type = 'ledger_type'; break;
+        }
+    }
+    $results = Data::search('BudgetData', $type.'=\''.$name.'\'');
+    $data = array();
+    foreach($results as $item){
+        if($deep){
+            $data[$item->get('year')] += (float)$item->get('amount');
+        }else{
+            $data[$item->get($uniqueType)][$item->get('year')] += (float)$item->get('amount');
+        }
+    }
+    $renderer->assign('data', $data);
 ?>
