b6f0d6450725b290b81e812f8ef634caead22f98
khrome <abbey@khrome.net>
2012-05-15 10:54:32 -0700

    updated graphs so backend reflects segmentation

--- App/Controllers/graph.controller.php
+++ App/Controllers/graph.controller.php
@@ -2,12 +2,17 @@
     //todo: add filtering by other columns
     $incomingType = WebApplication::get('type');
     $values = array();
+    $unique = uniqueCategories();
     $values['fund'] = WebApplication::get('fund')?WebApplication::get('fund'):false;
     $values['department'] = WebApplication::get('department')?WebApplication::get('department'):false;
     $values['category'] = WebApplication::get('category')?WebApplication::get('category'):false;
+    $mapping = json_decode(file_get_contents('Data/map.json'), true);
+    $data = $unique;
+    $dependencies = categoryDependencies(&$data, $unique, $mapping, $values);
+    //echo('|'.$incomingType.'|'.$values[$incomingType].'|'); exit();
     if(!$values[$incomingType]) throw new Exception('no value for type :'.$incomingType);
     $name = $values[$incomingType];
-    $deep = strstr($name, ':')?true:false;
+    $deep = strstr($name, ':')?true:false; //is this a second level request (aggregate)
     switch($incomingType = WebApplication::get('type')){
         case 'fund': $uniqueType = 'superfund_fund'; break;
         case 'department': $uniqueType = 'department_division'; break;
@@ -25,12 +30,22 @@
     $results = Data::search('BudgetData', $type.'=\''.$name.'\'');
     $data = array();
     foreach($results as $item){
-        if($item->get('ledger_type') == 'Expense') $transactionType = 'expenses';
-        if($item->get('ledger_type') == 'Revenue') $transactionType = 'revenue';
-        if($deep){
-            $data[$name][$item->get('year')][$transactionType] += (float)$item->get('amount');
-        }else{
-            $data[$item->get($uniqueType)][$item->get('year')][$transactionType] += (float)$item->get('amount');
+        //print_r($dependencies);
+        //print_r($item); exit();
+        if(
+            in_array($item->get('superfund_fund'), $dependencies['funds']) ||
+            in_array($item->get('department_division'), $dependencies['departments']) ||
+            in_array($item->get('ledger_type_ledger_description'), $dependencies['categories'])
+        ){
+            if($item->get('ledger_type') == 'Expense') $transactionType = 'expenses';
+            if($item->get('ledger_type') == 'Transfers In') $transactionType = 'transfersin';
+            if($item->get('ledger_type') == 'Transfers Out') $transactionType = 'transfersout';
+            if($item->get('ledger_type') == 'Revenue') $transactionType = 'revenue';
+            if($deep){
+                $data[$name][$item->get('year')][$transactionType] += (float)$item->get('amount');
+            }else{
+                $data[$item->get($uniqueType)][$item->get('year')][$transactionType] += (float)$item->get('amount');
+            }
         }
     }
     $renderer->assign('data', $data);
--- setup.php
+++ setup.php
@@ -7,6 +7,10 @@
     ResourceBundle::$minify = false; //encode the script
     ResourceBundle::$merge = true; //combine the scripts/styles
     
+    function startsWith($haystack, $needle){
+        return (substr($haystack, 0, strlen($needle)) === $needle);
+    }
+    
     function uniqueCategories(){
         $results = Data::search('BudgetData');
         $data = array(
@@ -25,6 +29,82 @@
             if(!in_array($result->get('department'), $data['departments'])) $data['departments'][] = $result->get('department');
             if(!in_array($dep, $data['departments']))
                 $data['departments'][] = $dep;
+        }
+        return $data;
+    }
+    
+    function categoryDependencies(&$data, $unique, $dependencyTree, $values){
+        $fund = $values['fund'];
+        $category = $values['category'];
+        $department = $values['department'];
+        $results = $dependencyTree;
+        //handle category selection
+        if( ($category && $category != '*') ){
+            $data['categories'] = array();
+            foreach ($unique['categories'] as $name){
+                if(startsWith($name, $category)){
+                    $data['categories'][] = $name;
+                }
+            }
+            $data['categories'] = array_values($data['categories']);
+            foreach($data['funds'] as $key => $fundItem){
+                if( !in_array( $fundItem, $results['ledgers'][$category]['funds'] ) ){
+                    unset($data['funds'][$key]);
+                }
+            }
+            $data['funds'] = array_values($data['funds']);
+            foreach($data['departments'] as $key => $departmentItem){
+                if( !in_array( $departmentItem, $results['ledgers'][$category]['divisions'] ) ){
+                    unset($data['departments'][$key]);
+                }
+            }
+            $data['departments'] = array_values($data['departments']);
+        }
+        
+        //handle fund selection
+        if( ($fund && $fund != '*') ){
+            $data['funds'] = array();
+            foreach ($unique['funds'] as $name){
+                if(startsWith($name, $fund)){
+                    $data['funds'][] = $name;
+                }
+            }
+            $data['funds'] = array_values($data['funds']);
+            foreach($data['categories'] as $key => $item){
+                if( !in_array( $item, $results['funds'][$fund]['ledgers'] ) ){
+                    unset($data['categories'][$key]);
+                }
+            }
+            $data['categories'] = array_values($data['categories']);
+            foreach($data['departments'] as $key => $item){
+                if( !in_array( $item, $results['funds'][$fund]['divisions'] ) ){
+                    unset($data['departments'][$key]);
+                }
+            }
+            $data['departments'] = array_values($data['departments']);
+        }
+        
+        //handle department selection
+        if( ($department && $department != '*') ){
+            $data['departments'] = array();
+            foreach ($unique['departments'] as $name){
+                if(startsWith($name, $department)){
+                    $data['departments'][] = $name;
+                }
+            }
+            $data['departments'] = array_values($data['departments']);
+            foreach($data['categories'] as $key => $item){
+                if( !in_array( $item, $results['divisions'][$department]['ledgers'] ) ){
+                    unset($data['categories'][$key]);
+                }
+            }
+            $data['categories'] = array_values($data['categories']);
+            foreach($data['funds'] as $key => $item){
+                if( !in_array( $item, $results['divisions'][$department]['funds'] ) ){
+                    unset($data['funds'][$key]);
+                }
+            }
+            $data['funds'] = array_values($data['funds']);
         }
         return $data;
     }
