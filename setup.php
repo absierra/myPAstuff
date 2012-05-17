20169f6e6a46d5792b5925795a36cd73cd8e839a
khrome <abbey@khrome.net>
2012-05-17 14:38:12 -0700

    fixed segmentation issue

--- setup.php
+++ setup.php
@@ -38,30 +38,15 @@
         $category = $values['category'];
         $department = $values['department'];
         $results = $dependencyTree;
-        //handle category selection
-        /*if( ($category && $category != '*') ){ deprecated
-            $data['categories'] = array();
-            foreach ($unique['categories'] as $name){
-                if(startsWith($name, $category)){
-                    $data['categories'][] = $name;
+        //handle selection possibilities
+        if( ($department && $department != '*') ){
+            $data['departments'] = array();
+            foreach ($unique['departments'] as $name){
+                if(startsWith($name, $department)){
+                    $data['departments'][] = $name;
                 }
             }
-            $data['categories'] = array_values($data['categories']);
-            foreach($data['funds'] as $key => $fundItem){
-                if( !in_array( $fundItem, $results['ledgers'][$category]['funds'] ) ){
-                    unset($data['funds'][$key]);
-                }
-            }
-            $data['funds'] = array_values($data['funds']);
-            foreach($data['departments'] as $key => $departmentItem){
-                if( !in_array( $departmentItem, $results['ledgers'][$category]['divisions'] ) ){
-                    unset($data['departments'][$key]);
-                }
-            }
-            $data['departments'] = array_values($data['departments']);
-        }*/
-        
-        //handle fund selection
+        }
         if( ($fund && $fund != '*') ){
             $data['funds'] = array();
             foreach ($unique['funds'] as $name){
@@ -69,6 +54,9 @@
                     $data['funds'][] = $name;
                 }
             }
+        }
+        //handle fund selection
+        if( ($fund && $fund != '*') ){
             $data['funds'] = array_values($data['funds']);
             foreach($data['categories'] as $key => $item){
                 if( !in_array( $item, $results['funds'][$fund]['ledgers'] ) ){
@@ -94,12 +82,6 @@
         
         //handle department selection
         if( ($department && $department != '*') ){
-            $data['departments'] = array();
-            foreach ($unique['departments'] as $name){
-                if(startsWith($name, $department)){
-                    $data['departments'][] = $name;
-                }
-            }
             $data['departments'] = array_values($data['departments']);
             foreach($data['categories'] as $key => $item){
                 if( !in_array( $item, $results['divisions'][$department]['ledgers'] ) ){
