cc270564c2fe9825c288b34f8d82c057993bb315
Nate Levine <nlevine@delphi.us>
2012-06-13 13:41:26 -0700

    graph colors

--- Resources/core/js/graph-util.js
+++ Resources/core/js/graph-util.js
@@ -36,10 +36,11 @@ PseudoDOM.after = function(element, properties){
 PseudoDOM.clear = function(element, properties){
     document.getElements('.pseudo-chop').destroy();
 };
-function hueShiftedColorSet(){
-    var args = {};
-    var arg;
-    for(var lcv=0; lcv < arguments.length; lcv++){
+function hueShiftedColorSet(num, categories){
+    //var args = {};
+    //var arg;
+    
+    /*for(var lcv=0; lcv < arguments.length; lcv++){
         arg = arguments[lcv];
         if(typeOf(arg) == 'number') args.num = arg;
         else if(
@@ -47,9 +48,9 @@ function hueShiftedColorSet(){
             (typeOf(arg) == 'string' && arg.match(/#?[0-9A-F]{6}/))
         ) args.color = arg;
         else if(typeOf(arg) == 'string') args.result = arg;
-    };
-    if(!args.num) args.num = 5;
-    if(!args.color) args.color = '#336699';
+    };*/
+    //if(!args.num) args.num = 5;
+    //if(!args.color) args.color = '#336699';
     /*var result = [];
     for(var lcv=0;lcv<args.num;lcv++){
     	console.log(args.color);
@@ -60,12 +61,19 @@ function hueShiftedColorSet(){
     }
     console.log(result);
     return result;*/
+    var colorMap = {'Expenses':'#F3715B', 'Revenues':'#00B039', 'Allocated Charges':'#5578B4', 'Contract Services':'#AB72A6', 'Debt Service':'#F3715B', 'General Expense':'#00B039', 'Rents & Leases':'#F79750', 'Salaries & Benefits':'#A9CEEC', 'Supplies & Materials':'#CACEC3', 'Facilities & Equip Purchases':'#AAA7CA', 'Wastewater Treatment':'#66C190', 'Utility Purchases & Charges':'#DF8B96', 'Capital Improvement':'#68B3E2', 'Charges for Services':'#5578B4', 'Net Sales':'#AB72A6', 'Other Income':'#F3715B', 'Other Revenue':'#00B039', 'Return on Investments':'#F79750', 'Charges to Other Funds':'#A9CEEC', 'From Other Agencies':'#CACEC3', 'Permits and Licenses':'#AAA7CA', 'Other Taxes and Fines':'#66C190', 'Property Taxes':'#DF8B96', 'Rental Income':'#68B3E2', 'Sales Taxes':'#FFDB4E'};
+    
+    if (categories){
+    	var catColors = [];
+    	Object.each(categories, function(category){
+    		catColors.push(colorMap[category]);
+    	});
+    	return catColors;
+    }
 
-    var colors = ['#00CCFF', '#FF9900', '#99CC00', '#FF33FF', '#FFFF66', '#FF3333', '#3333FF', '#996633', '#339933', '#FF6600', '#FF0099', '#00CCFF', '#FF9900', '#99CC00', '#FF33FF', '#FFFF66', '#FF3333', '#3333FF', '#996633', '#339933', '#FF6600', '#FF0099', '#00CCFF', '#FF9900', '#99CC00', '#FF33FF', '#FFFF66', '#FF3333', '#3333FF', '#996633', '#339933', '#FF6600', '#FF0099','#CB0077', '#A2EF00', '#699B00', '#84004D', '#00A580', '#35D2AF', '#FF6200', '#FF8940', '#1046AA', '#4577D4', '#FFA500', '#FFBC40', '#640BAC', '#9240D5', '#FFF500', '#FFF840', '#F10026', '#F83E5B', '#3BDA00', '#6BED3B', '#FF6400', '#FF8B40', '#00A483', '#34D1B3', '#FF8700', '#FFA540', '#024A69', '#0773A1', '#FFC200', '#FFDD73', '#0C0847', '#7672D9', '#FFE800', '#FFF273', '#330571', '#996AD6', '#E2FA00', '#EFFD72', '#58026E', '#BE63D4', '#92EC00', '#C2F56E', '#890043', '#E969A8', '#009A9A', '#5CCCCC', '#A64B00', '#FFB273', '#0B5FA5', '#66A1D2', '#A66000', '#FFAE40', '#1435AE', '#061D71', '#FFD473', '#BF9430', '#530EAE', '#330571', '#FFEE40', '#BFB230'];
-
-    //var colors = ['#00CCFF', '#FF9900', '#99CC00', '#FF33FF', '#FFFF66', '#FF3333', '#3333FF', '#996633', '#339933', '#FF6600', '#FF0099', '#00CCFF', '#FF9900', '#99CC00', '#FF33FF', '#FFFF66', '#FF3333', '#3333FF', '#996633', '#339933', '#FF6600', '#FF0099', '#00CCFF', '#FF9900', '#99CC00', '#FF33FF', '#FFFF66', '#FF3333', '#3333FF', '#996633', '#339933', '#FF6600', '#FF0099'];
-    //var colors = ['#2E67B2', '#99CA3C', '#673466', '#FFCD32', '#EE1D3A', '#603913', '#FFCD05', '#8C3694', '#996632', '#50C3C6', '#F89838', '#991C20', '#39B54A', '#F26837', '#EE2A66'];
-    return colors.slice(0, args.num);
+    //var colors = ['#00CCFF', '#FF9900', '#99CC00', '#FF33FF', '#FFFF66', '#FF3333', '#3333FF', '#996633', '#339933', '#FF6600', '#FF0099', '#00CCFF', '#FF9900', '#99CC00', '#FF33FF', '#FFFF66', '#FF3333', '#3333FF', '#996633', '#339933', '#FF6600', '#FF0099', '#00CCFF', '#FF9900', '#99CC00', '#FF33FF', '#FFFF66', '#FF3333', '#3333FF', '#996633', '#339933', '#FF6600', '#FF0099','#CB0077', '#A2EF00', '#699B00', '#84004D', '#00A580', '#35D2AF', '#FF6200', '#FF8940', '#1046AA', '#4577D4', '#FFA500', '#FFBC40', '#640BAC', '#9240D5', '#FFF500', '#FFF840', '#F10026', '#F83E5B', '#3BDA00', '#6BED3B', '#FF6400', '#FF8B40', '#00A483', '#34D1B3', '#FF8700', '#FFA540', '#024A69', '#0773A1', '#FFC200', '#FFDD73', '#0C0847', '#7672D9', '#FFE800', '#FFF273', '#330571', '#996AD6', '#E2FA00', '#EFFD72', '#58026E', '#BE63D4', '#92EC00', '#C2F56E', '#890043', '#E969A8', '#009A9A', '#5CCCCC', '#A64B00', '#FFB273', '#0B5FA5', '#66A1D2', '#A66000', '#FFAE40', '#1435AE', '#061D71', '#FFD473', '#BF9430', '#530EAE', '#330571', '#FFEE40', '#BFB230'];
+	var colors = ['#5578B4', '#AB72A6', '#F3715B', '#00B039', '#F79750', '#A9CEEC', '#CACEC3', '#AAA7CA', '#66C190', '#DF8B96', '#68B3E2', '#FFDB4E', '#96D4C3', '#A7A9AC', '#BBBA59', '#E7A7CB', '#996633'];
+    return colors.slice(0, num);
 }
 /*
 (function(context){
